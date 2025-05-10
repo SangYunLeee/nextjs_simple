4f1d04ac555352aacdfff8e42594f34f1118b5c3
2sangyun <sororiri@gmail.com>
2025-05-10 13:21:43 +0900

    빌드 에러 수정

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import Header from "@/components/header";
-export default async function Home({params}: {params: {slug: string}}) {
+
+interface BlogPageProps {
+  params: Promise<{ slug?: string }>;
+}
+
+export default async function BlogPage({params}: BlogPageProps) {
   const { slug } = await params;
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
